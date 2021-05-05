264898a064f11f7a7f78e660e918d53da7085b8f
Hudson Marinho <hudsonmarinhodev@gmail.com>
2021-05-05 04:29:43 -0300

    Create Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { useApp } from '../../hooks/AppContext';
 
-const Home: React.FC = () => {
-  return <h1>Frontend Challenge</h1>;
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+const Home: React.FC<RouteComponentProps> = ({ history }) => {
+  const { isLoading, getYourMaster, location } = useApp();
+
+  useEffect(() => {
+    if (location !== '/') {
+      history.push(location);
+    }
+  }, [location, history]);
+
+  return (
+    <Container>
+      <div>
+        <Title>
+          Welcome to <strong>iClinic</strong>
+        </Title>
+
+        <h5>Frontend Challenge</h5>
+      </div>
+
+      <Button onClick={() => getYourMaster()} loading={isLoading} type="button">
+        Start
+      </Button>
+    </Container>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,64 @@
 import styled from 'styled-components';
+import { breakpoints, colors, fonts } from '../../styles/tokens';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  min-height: 350px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  color: ${colors.primary};
+
+  h5 {
+    font-weight: ${fonts.weights.normal};
+    font-size: 12px;
+    letter-spacing: 0.35em;
+    text-transform: uppercase;
+    transition: font-size 0.2s;
+    animation: fadeInDown;
+    animation-duration: 0.9s;
+
+    @media (min-width: ${breakpoints.mobile}) {
+      font-size: 14px;
+    }
+  }
+
+  button {
+    width: 189px;
+    height: 56px;
+    margin-top: 60px;
+    letter-spacing: 6px;
+    text-transform: uppercase;
+    font-size: 14px;
+    animation: fadeInDown;
+    animation-duration: 1s;
+
+    @media (min-width: ${breakpoints.mobile}) {
+      margin-top: 162px;
+      font-size: 18px;
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 7px;
+  font-size: 40px;
+  font-weight: ${fonts.weights.normal};
+  animation: fadeInDown;
+  animation-duration: 0.8s;
+  transition: font-size 0.2s;
+
+  @media (min-width: ${breakpoints.phone}) {
+    font-size: 58px;
+  }
+
+  @media (min-width: ${breakpoints.mobile}) {
+    font-size: 72px;
+  }
+
+  strong {
+    font-weight: ${fonts.weights.bold};
+  }
+`;
